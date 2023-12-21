c2a22213efe88f91075bd1b149c7a0431d71b888
Abhishek Alimchandani <abhishekalimchandani@pop-os.localdomain>
2023-12-21 21:03:37 +0530

    Test

---
+++ Backend/contracts/Test/ETHERC20.sol
@@ -0,0 +1,21 @@
+// SPDX-License-Identifier: SEE LICENSE IN LICENSE
+pragma solidity 0.8.19;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract ETHERC20 is ERC20 {
+
+    address immutable i_owner;
+
+    constructor() ERC20("ETHERC20","ETH") {
+        i_owner = msg.sender;
+        mint(msg.sender, 1000 * 10 ** decimals());
+    }
+
+    function mint(address to, uint256 amount) public {
+        if (msg.sender != i_owner) {
+            revert ();
+        }
+        _mint(to, amount);
+    }
+}
